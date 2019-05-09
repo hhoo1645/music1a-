2cb28697b99a49afb5b06076bb4245c7bc591467
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2019-05-09 23:35:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -214,7 +214,7 @@ function play(guild, song) {
 }
 
 const adminprefix = "1vip";
-const devs = ['407244267984125972','407244267984125972']
+const devs = ['407244267984125972','380254757455134725']
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
